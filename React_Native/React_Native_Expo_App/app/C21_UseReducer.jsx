e8b5e5695ce02383432ce4cb23302d44c4896fd5
Vijay Kumar <Be1newinner@gmail.com>
2024-06-20 18:12:45 +0530

    added C23_Redux

--- React_Native/React_Native_Expo_App/app/C21_UseReducer.jsx
+++ React_Native/React_Native_Expo_App/app/C21_UseReducer.jsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function C21_UseReducer() {
   return (
     <View>
-      <Text>C21_UseReducer</Text>
+      <Text>UseReducer</Text>
     </View>
-  )
+  );
 }
--- React_Native/React_Native_Expo_App/app/_layout.jsx
+++ React_Native/React_Native_Expo_App/app/_layout.jsx
@@ -1,10 +1,14 @@
 import { Drawer } from "expo-router/drawer";
 import { CartContextProvider } from "../service/CartService/CartContext";
+import { Provider } from "react-redux";
+import store from "../service/Store/store";
 
 export default function Layout() {
   return (
-    <CartContextProvider>
-      <Drawer screenOptions={{}} />
-    </CartContextProvider>
+    <Provider store={store}>
+      <CartContextProvider>
+        <Drawer screenOptions={{}} />
+      </CartContextProvider>
+    </Provider>
   );
 }
