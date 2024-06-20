764dd8f6b0a82579cabdfd8df7ee7d75a95b8677
Vijay Kumar <Be1newinner@gmail.com>
2024-06-20 20:49:15 +0530

    saved

--- React_Native/react_native_expo_navigation/expo_drawer_navigation/app/_layout.jsx
+++ React_Native/react_native_expo_navigation/expo_drawer_navigation/app/_layout.jsx
@@ -1,136 +1,44 @@
-import { Ionicons } from "@expo/vector-icons";
-import {
-  DrawerContentScrollView,
-  DrawerItem,
-  DrawerItemList,
-} from "@react-navigation/drawer";
+import React from "react";
 import { Drawer } from "expo-router/drawer";
-import { Dimensions, Text, TextInput, View } from "react-native";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Layout() {
+export default function _layout() {
   return (
     <Drawer
       screenOptions={{
-        // header: () => (
-        //   <View
-        //     style={{
-        //       marginTop: 20,
-        //       height: 50,
-        //       backgroundColor: "red",
-        //     }}
-        //   >
-        //     <Text>Header</Text>
-        //   </View>
-        // ),
-        // title: "Nothibg",
-        // drawerLabel: "Label Something",
-        // drawerLabel: (e) => <Text>{e + " Page"}</Text>,
-        // drawerActiveTintColor: "green",
-        // drawerActiveBackgroundColor: "skyblue",
-        // drawerInactiveTintColor
-        // drawerInactiveBackgroundColor: "green",
-        // drawerItemStyle: {
-        //   backgroundColor: "pink",
-        //   elevation: 5,
-        //   height: 100,
-        // },
-        // drawerLabelStyle: {
-        //   color: "blue",
-        //   backgroundColor: "red",
-        // },
-        // drawerContentContainerStyle: {
-        // gap: 50,
-        // backgroundColor: "red",
-        // width: 300,
-        // },
-        // drawerContentStyle: {
-        // backgroundColor: "blue",
-        // width: Dimensions.get("screen").width,
-        // },
-        // drawerStyle: {
-        //   backgroundColor: "pink",
-        //   width: Dimensions.get("screen").width,
-        // },
-        // drawerPosition: "right",
-        // drawerType: "front",
-        // drawerHideStatusBarOnOpen: true,
-        // overlayColor:"red"
-        swipeEnabled: true,
         // headerShown: false,
-        drawerIcon: () => <Ionicons name="alarm" size={40} />,
+        // header: () => {
+        //   return (
+        //     <View
+        //       style={{
+        //         height: 100,
+        //         backgroundColor: "red",
+        //       }}
+        //     >
+        //       <Text>Header</Text>
+        //     </View>
+        //   );
+        // },
+        title: "Something",
+        drawerLabel: "Page1",
+        drawerActiveTintColor: "red",
+        drawerInactiveTintColor: "blue",
+        drawerActiveBackgroundColor: "orange",
+        drawerInactiveBackgroundColor: "pink",
+        drawerLabelStyle: {
+          fontSize: 18,
+          fontWeight: 900,
+        },
+        drawerItemStyle: {
+          borderRadius: 200,
+        },
+        // swipeEnabled: false,
+        drawerIcon: () => <Ionicons name="apps-outline" />,
       }}
-      drawerContent={(props) => {
-        // console.log(props);
-        // console.log(Object.keys(props.state.index));
-        const data = props.state.routes;
-        return (
-          <DrawerContentScrollView>
-            <View
-              style={{
-                padding: 10,
-              }}
-            >
-              <TextInput
-                style={{
-                  borderWidth: 1,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  paddingVertical: 5,
-                }}
-                placeholder="Search Items"
-              />
-            </View>
-            <DrawerItemList {...props} />
-            {/* {props.state.routes
-              .filter((item) => {
-                return item.name != "about";
-              })
-              .map((item) => {
-                console.log(item);
-                return (
-                  <View key={item.key}>
-                    <Text>{item.name}</Text>
-                  </View>
-                );
-              })} */}
-            <DrawerItem label={"Log Out"} onPress={() => alert("hi")} />
-          </DrawerContentScrollView>
-        );
-      }}
-    >
-      <Drawer.Screen
-        name="index"
-        options={{
-          title: "Home",
-          headerShown: true,
-          drawerLabel: "Home Page",
-          drawerIcon: () => <Ionicons name="baseball" size={40} />,
-        }}
-      />
-    </Drawer>
+    />
   );
 }
 
 // Install first this
 // npx expo install @react-navigation/drawer react-native-gesture-handler react-native-reanimated
-
-// drawerContent={(props) => (
-//   <DrawerContentScrollView {...props}>
-//     <View
-//       style={{
-//         padding: 10,
-//       }}
-//     >
-//       <TextInput
-//         style={{
-//           borderWidth: 1,
-//           borderRadius: 5,
-//           paddingHorizontal: 10,
-//           paddingVertical: 5,
-//         }}
-//         placeholder="Search Items"
-//       />
-//     </View>
-//     <DrawerItemList {...props} />
-//   </DrawerContentScrollView>
-// )}
