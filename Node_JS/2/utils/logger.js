f1f8dd9d05004c1ba75b5832ce4408699de686cf
Vijay Kumar <Be1newinner@gmail.com>
2024-07-04 15:46:40 +0530

    building fake api for Vercel Hosting

--- Node_JS/2/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-
-const logger = morgan('combined', { stream: accessLogStream });
-
-module.exports = logger;
