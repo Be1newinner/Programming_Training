b009d275339b12482f04d36ee8a686754780c38a
Vijay Kumar <Be1newinner@gmail.com>
2024-07-04 22:54:00 +0530

    modify fakeAPIDemo

--- Node_JS/Nodejs-swagger-api/index.js
+++ Node_JS/Nodejs-swagger-api/index.js
@@ -7,18 +7,15 @@ import swaggerJsDoc from "swagger-jsdoc";
 
 import bodyParser from "body-parser";
 
-// Import the router from the hello.js file
 import postRouter from "./src/Routes/posts.js";
 import helloRouter from "./src/hello.js";
-
-// CDN CSS
 
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const app = express();
 
-app.use(bodyParser.json()); // to use body object in requests
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 2001;
 dotenv.config();
 
@@ -46,12 +43,10 @@ const options = {
       },
     ],
   },
-  // This is to call all the file
   apis: ["src/**/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(
   "/api-docs",
@@ -59,83 +54,7 @@ app.use(
   swaggerUI.setup(specs, { customCssUrl: CSS_URL })
 );
 
-// Here we are calling the basic html
-// Use the router from the hello.js file
 app.use("/", helloRouter);
-// Use the router from the post.js file
 app.use("/posts", postRouter);
 
 app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
-
-// import express from "express";
-// import cors from "cors";
-// import morgan from "morgan";
-// import dotenv from "dotenv";
-// import bodyParser from "body-parser";
-// import swaggerUI from "swagger-ui-express";
-// import swaggerJsDoc from "swagger-jsdoc";
-
-// // Import the router from the hello.js file
-// import postRouter from "./src/Routes/posts.js";
-// import helloRouter from "./src/hello.js";
-
-// //? CDN CSS
-// const CSS_URL =
-//   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-// const app = express();
-
-// app.use(bodyParser.json()); // to use body object in requests
-// const PORT = process.env.PORT || 2000;
-// dotenv.config();
-
-// app.use(morgan("dev"));
-// app.use(cors());
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Library API",
-//       version: "1.0.0",
-//       description: "A simple Express Library API",
-//       termsOfService: "http://example.com/terms/",
-//       contact: {
-//         name: "API Support",
-//         url: "http://www.exmaple.com/support",
-//         email: "support@example.com",
-//       },
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:2000",
-//         description: "My API Documentation",
-//       },
-//     ],
-//   },
-//   // This is to call all the file
-//   apis: ["src/**/*.js"],
-// };
-
-// const specs = swaggerJsDoc(options);
-// // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-// // custome css added!
-// app.use("/public", express.static("public/swagger.css"));
-// app.use(
-//   "/api-docs",
-//   swaggerUI.serve,
-//   swaggerUI.setup(specs, { customCssUrl: "/public/swagger.css" && CSS_URL })
-// );
-
-// // Here we are calling the basic html
-// // Use the router from the hello.js file
-
-// //* All the routes goes here!
-// app.use("/", helloRouter);
-
-// // Use the router from the post.js file
-// app.use("/posts", postRouter);
-
-// //? This is for running the app
-// app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
--- Node_JS/Nodejs-swagger-api/src/hello.js
+++ Node_JS/Nodejs-swagger-api/src/hello.js
@@ -1,13 +1,3 @@
-// export default router;
-// router.get("/", (req, res) => {
-//   res.render("pages/index");
-// });
-
-// Getting the index html file here ejs
-// router.get("/", (req, res) => {
-//   res.render("src/pages/index");
-// });
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -17,20 +7,17 @@ import path from "path";
 
 const router = express();
 
-router.use(bodyParser.json()); // to use body object in requests
+router.use(bodyParser.json());
 router.use(morgan("dev"));
 router.use(cors());
 
 router.set("view engine", "ejs");
-
-// This was we can keep everything inside our src folder!!
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 router.set("views", path.join(__dirname, "views"));
 router.set("view engine", "ejs");
 
-// This is to read csss
 router.use(express.static(path.join(__dirname, "views/pages")));
 
 router.get("/", (req, res) => {
