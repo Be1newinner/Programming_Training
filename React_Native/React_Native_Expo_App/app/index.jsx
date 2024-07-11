0e384a1e91f14a497298bded9892b38d01b843c9
Vijay Kumar <Be1newinner@gmail.com>
2024-07-11 17:09:00 +0530

    Added ViewPager

--- React_Native/React_Native_Expo_App/app/index.jsx
+++ React_Native/React_Native_Expo_App/app/index.jsx
@@ -1,22 +1,7 @@
-// import { Redirect } from "expo-router";
-import { Text, View, ViewBase } from "react-native";
-import C31_DateTimePicker from "./C31_DateTimePicker";
+import C32_ViewPager from "./C32_ViewPager";
 
 function App() {
-  return (
-    <View
-      style={{
-        padding: 20,
-      }}
-    >
-      {/* <Text>Welcome to Asha Learnology</Text>
-      <Text>React Native Course</Text>
-      <Text>by Vijay Sir</Text>
-      <Text>for support contact me at be1newinner@gmail.com</Text> */}
-
-      <C31_DateTimePicker />
-    </View>
-  );
+  return <C32_ViewPager />;
 }
 
 export default App;
