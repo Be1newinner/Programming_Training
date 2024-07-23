5a10ee599147aac58bbe897f04ad4473f49864dd
Vijay Kumar <Be1newinner@gmail.com>
2024-07-23 14:05:28 +0530

    Added React_ContextAPI

---
+++ React_JS/projects/react_context_api/src/app/HomePage/index.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Link to="/second"> Second Page with Link </Link>
+    </div>
+  );
+}
---
+++ React_JS/projects/react_context_api/src/app/SecondPage/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function SecondPage() {
+  return (
+    <div>
+      <Link to="/">
+        Home Page with Link
+      </Link>
+    </div>
+  );
+}
