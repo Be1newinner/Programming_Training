4c1ecc3420febf842d7cdc40083813205c00d898
Vijay Kumar <Be1newinner@gmail.com>
2024-07-31 06:11:34 +0530

    updated C36_useMemo in react Native

--- React_Native/projects/training_sessions/app/C36_UseMemo.jsx
+++ React_Native/projects/training_sessions/app/C36_UseMemo.jsx
@@ -3,7 +3,7 @@ import { View, Text, Button } from "react-native";
 
 // Component that calculates factorial of a number
 const Factorial = ({ number }) => {
-  const factorial = (n) => {
+  const factorialCalulcation = (n) => {
     let result = 1;
     for (let i = 1; i <= n; i++) {
       result *= i;
@@ -12,12 +12,15 @@ const Factorial = ({ number }) => {
     return result;
   };
 
-  const memoizedFactorial = useMemo(() => factorial(number), [number]);
+  const memoizedFactorial = useMemo(
+    () => factorialCalulcation(number),
+    [number]
+  );
 
   return (
     <View>
       {/* <Text>
-        Factorial of {number}: {factorial(number)}
+        Factorial of {number}: {factorialCalulcation(number)}
       </Text> */}
       <Text>
         Factorial of {number}: {memoizedFactorial}
@@ -33,7 +36,12 @@ const ParentComponent = () => {
   return (
     <View>
       <Factorial number={number} />
-      <Button title="Increment Count" onPress={() => setCount(count + 1)} />
+      <Button
+        title="Increment Count"
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      />
       <Button title="Change Number" onPress={() => setNumber(number + 1)} />
       <Text>Count: {count}</Text>
     </View>
