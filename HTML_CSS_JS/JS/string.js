808bf3725b417c9a9d1624fe8331553e5a257528
Vijay Kumar <Be1newinner@gmail.com>
2024-08-14 14:03:10 +0530

    Adding Angular Counter_APP

---
+++ HTML_CSS_JS/JS/string.js
@@ -0,0 +1,21 @@
+const str = "         VIJAY_VIJAY_KUMAR           ";
+
+// const result = str.charAt(6);
+// const result = str.charCodeAt(6);
+// const result = str.endsWith("MAR");
+// const result = str.endsWith("MAR");
+// const result = String.fromCharCode(76);
+// const result = str.padEnd(20,"A");
+// const result = str.padStart(3, "0");
+// const result = Number(str.padStart(3, "0"));
+// const result = str.repeat(5);
+// const result = str.replace("VIJAY", "MR.");
+// const result = str.replaceAll("VIJAY", "MR.");
+const result = str.trim();
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   console.log(str.charAt(2));
+// }
+
+console.log(result);
