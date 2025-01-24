9a24bd925c30c1a5c1846020ef17f0473e6e2b8e
Vijay Kumar <Be1newinner@gmail.com>
2025-01-24 15:23:49 +0530

    Batch 16 > Loops

---
+++ BATCH_16/loops.py
@@ -0,0 +1,44 @@
+# Range 0 to 9
+"""
+for i in range(10):
+   print(i)
+
+# Range 2 to 9
+for i in range(2,10):
+   print(i)
+
+# Range 2 to 30 with step 3
+for i in range(2,30,3):
+   print(i)
+   
+"""
+
+#while loop
+isRun = False
+i = 0
+
+while isRun == False:
+   print("Vijay")
+   i += 1
+   if(i > 5):
+      isRun = True
+
+
+"""
+Q.1
+Table print krni hai!
+output:
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+
+
+"""
