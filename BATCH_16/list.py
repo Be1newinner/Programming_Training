994ff8aa75754e067d9ea8e43027b1942584b312
Vijay <be1newinner@gmail.com>
2025-01-29 15:23:49 +0530

    Restructured and added all methods of List in list.py

--- BATCH_16/list.py
+++ BATCH_16/list.py
@@ -1,26 +1,95 @@
 # simple list
 
-names = ["Vijay", "Ajay", "Sanjay", "Arjun"]
+names = ["Vijay", "Ajay", "Sanjay", "Arjun", "Ajay"]
+ages = [15,12,19,18,20,25, 1, 100]
 
-# popped = names.pop()
+### Remove data from an List
+# popped = names.pop() # remove element based on index number
 # popped = names.pop(1)
 # print(popped)
+# names.remove("Ajay") # Remove element based on element name
+# print(names)
 
-# names.append("Karan")
 
-# names.insert(1, "Karan")
 
-names[1] = "Ajay Kumar"
+### Adding data to List
+# names.append("Karan") # Last me data ko add krta hai
+# names.insert(1, "Karan") # kisi bhi index pe add krne k liye
 
+
+
+### Update Data on an index number
+# names[1] = "Ajay Kumar"
+
+
+
+### Read element of an index number
 # print(names[1])
-print(names)
-print(len(names))
 
 
+
+### find length of the list
+# length_list = len(names)
+# print(length_list)
+# or
+# print(len(names))
+
+
+### List slicing
 # new_list = names[1:3]
 # new_list = names[1:]
 # new_list = names[:3]
 # new_list = names[:]
-
 # new_list = names[::2]
 # print(new_list)
+
+
+
+#### count number of occurence of an element
+# print(names.count("Arjun"))
+
+
+
+### find the index of an element
+# print(names.index("Sanjay"))
+
+
+
+### sort the List in Assending order based on alphanumeric casing
+# names.sort()
+# ages.sort()
+# print(names, ages)
+
+
+
+### Reverse the List 
+# names.reverse()
+# print(names)
+
+
+
+### sort the List in Descending order based on alphanumeric casing
+# names.sort()
+# names.reverse()
+# ages.sort()
+# ages.reverse()
+# print(names, ages)
+
+
+
+### Empty the List
+# names.clear()
+# print(names)
+
+
+
+### find minimum or maximum in a List
+# minimum = min(ages)
+# maximum = max(ages)
+# print(minimum, maximum)
+
+
+
+### Add all items in List
+# total_sum = sum(ages)
+# print(total_sum)
