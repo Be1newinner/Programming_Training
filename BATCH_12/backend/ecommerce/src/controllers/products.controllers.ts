2c916769392be7798ff2f5479975d1b106836011
Vijay <be1newinner@gmail.com>
2025-01-29 13:07:16 +0530

    Update products.controllers.ts

--- BATCH_12/backend/ecommerce/src/controllers/products.controllers.ts
+++ BATCH_12/backend/ecommerce/src/controllers/products.controllers.ts
@@ -5,19 +5,23 @@ export const ProductsListController = (req: Request, res: Response) => {
   try {
     const response = JSONResponse({
       status_code: 200,
-      message: "products fetched successfully!",
+      message: "Products fetched successfully!",
     });
 
     res.json(response);
     return;
-  } catch (error) {
-    console.log(error);
+  } catch (error: unknown) {
+    console.error(error);
+
+    const errMessage =
+      error instanceof Error ? error.message : "Unknown Error!";
+
     const response = JSONResponse({
-      status_code: 200,
-      message: error?.message || "Unknown Error!",
+      status_code: 500,
+      message: errMessage,
     });
 
-    res.json(response);
+    res.status(500).json(response);
     return;
   }
 };
