04125727e3aa9e677321eee78aca3c16ebd37e18
Vijay <be1newinner@gmail.com>
2025-02-07 15:45:58 +0530

    Update tupple.py

--- BATCH_16/tupple.py
+++ BATCH_16/tupple.py
@@ -1,33 +1,51 @@
-# names = ("Vijay", "Ajay", "Sanjay", "Arjun")
+names = ("Vijay", "Ajay", "Sanjay", "Arjun") # tupple of strings
+ages = (15,25,36,15) # tupple of integers
+persons = ([],[],[]) #tupple of list
+cars = ((2,5),(6,7),(8,9))
+
+# names = [1,4,5]
+
+# names[1] = 20
 # names[0] = "Karan"
 
+planets = ("MERCURY", "EARTH", "NEPTUNE")
+
+print(names[0])
+
+# CRUD
+
+# CREATE
+# READ
+# UPDATE => nahi
+# DELETE
 
 
 age1 = (1,0,3)
 age2 = (1,2,6)
 
 
-
 # ages = (25,20)
 # ages = (25,)
-
 
 
 ### find length of tuple
 # print(len(ages))
 
 
+len(planets)
+
 
 ### data type of tupple
 # print(type(ages))
 
 
-
 ### arithmetic operations
 # age3 = age1 + age2
-age3 = age1 * 3
+# age3 = age1 * 3
 
+planets2 = planets * 2
 
+print("MERCURY" in  planets2)
 
 ### Comparison
 # print(age1 < age2)
@@ -36,3 +54,12 @@ age3 = age1 * 3
 
 ### Check if an item exist in tupple
 # print(3 in age1 )
+
+
+
+'''
+Question. 1
+names = ["Vijay", "Ajay", "Sanjay"]
+
+
+'''
