1cfbb7bd46ee47cd7e2477cc5a47c6f36152d666
Vijay <be1newinner@gmail.com>
2025-02-08 17:26:41 +0530

    Update tupple.py

--- BATCH_16/tupple.py
+++ BATCH_16/tupple.py
@@ -1,16 +1,16 @@
-names = ("Vijay", "Ajay", "Sanjay", "Arjun") # tupple of strings
-ages = (15,25,36,15) # tupple of integers
-persons = ([],[],[]) #tupple of list
-cars = ((2,5),(6,7),(8,9))
+# names = ("Vijay", "Ajay", "Sanjay", "Arjun") # tupple of strings
+# ages = (15,25,36,15) # tupple of integers
+# persons = ([],[],[]) #tupple of list
+# cars = ((2,5),(6,7),(8,9))
 
 # names = [1,4,5]
 
 # names[1] = 20
 # names[0] = "Karan"
 
-planets = ("MERCURY", "EARTH", "NEPTUNE")
+# planets = ("MERCURY", "EARTH", "NEPTUNE")
 
-print(names[0])
+# print(names[0])
 
 # CRUD
 
@@ -20,8 +20,8 @@ print(names[0])
 # DELETE
 
 
-age1 = (1,0,3)
-age2 = (1,2,6)
+# age1 = (1,0,3)
+# age2 = (1,2,6)
 
 
 # ages = (25,20)
@@ -32,7 +32,7 @@ age2 = (1,2,6)
 # print(len(ages))
 
 
-len(planets)
+# len(planets)
 
 
 ### data type of tupple
@@ -43,9 +43,9 @@ len(planets)
 # age3 = age1 + age2
 # age3 = age1 * 3
 
-planets2 = planets * 2
+# planets2 = planets * 2
 
-print("MERCURY" in  planets2)
+# print("MERCURY" in  planets2)
 
 ### Comparison
 # print(age1 < age2)
@@ -58,8 +58,50 @@ print("MERCURY" in  planets2)
 
 
 '''
+
 Question. 1
 names = ["Vijay", "Ajay", "Sanjay"]
 
 
 '''
+
+# my_tupple = ()
+# names = ["Vijay", "Ajay", "Sanjay", "Vijay", "Sanjay"]
+
+# for x in names: 
+#    my_tupple = my_tupple + (x,)
+
+
+# print(my_tupple)
+
+# my_tuple = (2,)
+# print(type(my_tuple))
+
+### count and index
+
+# print(names.count("Sanjay"))
+# print(names.index("Vijay", 1))
+# print(names.index("Vijay"))
+
+
+
+# print(names[0:3:3])
+
+
+
+
+
+#### CONVERSION 
+# LIST => TUPLE
+
+names = ["Vijay", "Ajay"]
+ages = (1,5,6,7)
+
+
+names_tupp = tuple(names)
+ages_list = list(ages)
+	
+
+print(names_tupp)
+
+
